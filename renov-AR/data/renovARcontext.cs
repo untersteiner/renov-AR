34ceefdc5bc1976813395a6f99b8f8e907e6c5ad
untersteiner <alan.untersteiner@dirmod.com>
2021-06-03 15:06:16 -0300

    Agregado contexto de la base de datos

---
+++ renov-AR/data/renovARcontext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace renov_AR.data
+{
+    public class renovARcontext : DbContext
+    {
+        public renovARcontext(DbContextOptions<renovARcontext> options) : base(options)
+        {
+        }
+    }
+}
